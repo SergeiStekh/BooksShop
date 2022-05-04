c61d2571568dc4ab370d280dbf398e9b0c61dfc2
SergeyCodeJs <sergeysteh@gmail.com>
2022-05-04 10:50:36 +0200

    feat: add styles

--- script.js
+++ script.js
@@ -27,14 +27,14 @@ class BookStore {
     booksWrapper.classList.add("books");
 
     let ul = document.createElement('ul');
-    ul.classList.add("books-list");
+    ul.classList.add("books__list");
     ul.setAttribute("id", "books");
 
     let bagWrapper = document.createElement("div");
     bagWrapper.classList.add("bag");
 
     let bag = document.createElement("ul");
-    bag.classList.add("bag-list");
+    bag.classList.add("bag__list");
     bag.setAttribute("id", "bagList");
 
     bagWrapper.append(bag);
@@ -43,32 +43,50 @@ class BookStore {
       let li = document.createElement("li");
       li.classList.add("books__item");
 
-      let authorParagraph = document.createElement("p");
-      authorParagraph.classList.add("books__author");
-      authorParagraph.textContent = author;
-      li.append(authorParagraph);
-
-      let bookDescriptionParagraph = document.createElement("p");
-      bookDescriptionParagraph.classList.add("books__description");
-      bookDescriptionParagraph.textContent = description;
-      li.append(bookDescriptionParagraph);
+      let imageWrapper = document.createElement("div");
+      imageWrapper.classList.add("books__img-wrapper");
 
       let imageElement = document.createElement("img");
       imageElement.setAttribute("alt", title);
       imageElement.setAttribute("src", imageLink);
       imageElement.classList.add("books__img");
-      li.append(imageElement);
+      imageWrapper.append(imageElement);
+      li.append(imageWrapper);
 
-      let priceParagraph = document.createElement("p");
-      priceParagraph.classList.add("books__price");
-      priceParagraph.textContent = price;
-      li.append(priceParagraph);
+      let contentWrapper = document.createElement("div");
+      contentWrapper.classList.add("books__content-wrapper");
 
       let titleParagraph = document.createElement("p");
       titleParagraph.classList.add("books__title");
       titleParagraph.innerHTML = title;
-      li.append(titleParagraph);
+      contentWrapper.append(titleParagraph);
 
+      let authorParagraph = document.createElement("p");
+      authorParagraph.classList.add("books__author");
+      authorParagraph.textContent = author;
+      contentWrapper.append(authorParagraph);
+
+      // let bookDescriptionParagraph = document.createElement("p");
+      // bookDescriptionParagraph.classList.add("books__description");
+      // bookDescriptionParagraph.textContent = description;
+      // li.append(bookDescriptionParagraph);
+
+      let priceParagraph = document.createElement("p");
+      priceParagraph.classList.add("books__price");
+      priceParagraph.textContent = `${price} €`;
+      contentWrapper.append(priceParagraph);
+
+      let showMore = document.createElement("button");
+      showMore.classList.add("books__show-more");
+      showMore.textContent = "Show more";
+      contentWrapper.append(showMore);
+
+      let addToBag = document.createElement("button");
+      addToBag.classList.add("books__add-to-bag");
+      addToBag.textContent = "Add to bag";
+      contentWrapper.append(addToBag);
+
+      li.append(contentWrapper)
       ul.append(li);
     }
 
